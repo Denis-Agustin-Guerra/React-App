014044a49d6e94ab0327b9cebc416ca4356805b4
Denis-Agustin-Guerra <denisagustin.guerra@gmail.com>
2022-07-07 20:26:17 -0300

    ContadorRedux

---
+++ src/components/ContadorRed.tsx
@@ -0,0 +1,24 @@
+
+const initialState = {
+
+    contador:0,
+}
+
+type ActionType = { type: "incrementar"}
+
+const contadorReducer = (state: typeof initialState , action: ActionType) => {
+    
+
+    return state;
+
+
+
+}
+
+export const ContadorRed = () => {
+  return (
+    <>
+        <h2>Contador: 10</h2>
+    </>
+  )
+}
